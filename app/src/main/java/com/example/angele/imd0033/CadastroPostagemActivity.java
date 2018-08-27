897dff5b11164d0962e5afdd7cfc635d0e56416f
angelelouise <angelealst@hotmail.com>
2018-08-27 20:18:34 -0300

    fcm pronto

--- app/src/main/java/com/example/angele/imd0033/CadastroPostagemActivity.java
+++ app/src/main/java/com/example/angele/imd0033/CadastroPostagemActivity.java
@@ -1,23 +1,35 @@
 package com.example.angele.imd0033;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.angele.imd0033.dominio.ComponenteCurricular;
 import com.example.angele.imd0033.dominio.Postagem;
+import com.example.angele.imd0033.view.ComponenteViewModel;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CadastroPostagemActivity extends AppCompatActivity{
+public class CadastroPostagemActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     public static String INFO_EXTRA = "nova_postagem";
     private ViewHolder mViewHolder = new ViewHolder();
 
-
+    private ComponenteViewModel componenteViewModel;
+    private List<ComponenteCurricular> componentes;
+    Postagem  p = new Postagem();
 
     private static class ViewHolder{
         TextView mTituloView;
@@ -35,14 +47,35 @@ public class CadastroPostagemActivity extends AppCompatActivity{
         mViewHolder.mCCView = findViewById(R.id.spinnerCC);
         mViewHolder.mCadastrarNovaPostagem = findViewById(R.id.imageButtonSend);
 
+
+
+        componentes = new ArrayList<ComponenteCurricular>();
+        componenteViewModel = ViewModelProviders.of(this).get(ComponenteViewModel.class);
+        componenteViewModel.getListaComponentes().observe(this, new Observer<List<ComponenteCurricular>>() {
+            @Override
+            public void onChanged(@Nullable List<ComponenteCurricular> post) {
+                //postagemAdapter.setPalavras(post);
+                Log.d("post","post" +post);
+                componentes.clear();
+                componentes.addAll(post);
+            }
+        });
+        // Creating adapter for spinner
+        ArrayAdapter<ComponenteCurricular> dataAdapter = new ArrayAdapter<ComponenteCurricular>(this, android.R.layout.simple_spinner_item, componentes);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        mViewHolder.mCCView.setAdapter(dataAdapter);
         mViewHolder.mCadastrarNovaPostagem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                Postagem  p = new Postagem();
+
                 p.setDescricao(mViewHolder.mDescricaoView.getText().toString());
                 p.setTitulo(mViewHolder.mDescricaoView.getText().toString());
-                p.setId_componente_curricular((long) 0);
+                //p.setId_componente_curricular((long) 0);
                 intent.putExtra(INFO_EXTRA, (Serializable) p);
                 setResult(RESULT_OK,intent);
                 finish();
@@ -50,6 +83,19 @@ public class CadastroPostagemActivity extends AppCompatActivity{
         });
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+        ComponenteCurricular c = new ComponenteCurricular();
+        c = (ComponenteCurricular) parent.getItemAtPosition(position);
+        p.setId_componente_curricular((long) c.getId_componente());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 
 
 }
--- app/src/main/java/com/example/angele/imd0033/fmc/imd0033FirebaseMessageService.java
+++ app/src/main/java/com/example/angele/imd0033/fmc/imd0033FirebaseMessageService.java
@@ -1,9 +1,20 @@
 package com.example.angele.imd0033.fmc;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.angele.imd0033.MainActivity;
+import com.example.angele.imd0033.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -37,17 +48,44 @@ public class imd0033FirebaseMessageService extends FirebaseMessagingService {
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            final String msg = remoteMessage.getNotification().getBody();
+            //final String msg = remoteMessage.getNotification().getBody();
 
-            /*Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(new Runnable()){
-                public void run(){
-                    Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
-                }
-            }*/
         }
+
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        //String channelId = getString(R.string.default_notification_channel_id);
+        String channelId = "postagens";
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_menu_gallery)
+                        .setContentTitle("FCM Message")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
